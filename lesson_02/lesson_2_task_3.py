3cae1a632f8512501a6c8dcd356c1f21cec09a87
AntonKostenko86 <anton.kostenko.86@mail.ru>
2025-08-19 20:38:30 +0300

    Delete lesson_02 directory

--- lesson_02/lesson_2_task_3.py
+++
@@ -1,9 +0,0 @@
-import math
-
-
-def square(side):
-    return math.ceil(side * side)
-
-
-long_side = float(input("Введите длинну стороны квадрата: "))
-print(f"Площадь квадрата = {square(long_side)}")
